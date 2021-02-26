4b62e4b919d933bd4ececde5a5c37b5f8cfc6eb9
GectoR <dmitry.pustowoy@yandex.ru>
2021-02-26 16:51:09 +0300

    up

--- homebrew-delete.sh
+++ homebrew-delete.sh
@@ -2,3 +2,6 @@ sudo rm -rf /usr/local/Homebrew
 sudo rm -rf /usr/local/var/homebrew
 sudo rm -rf /System/Volumes/Data/usr/local/Homebrew
 sudo rm -rf /System/Volumes/Data/usr/local/var/homebrew
+
+
+find / -type d -name "*omebrew*"
